b932a8af5d63d96c435c9be4ddebab580277568d
Arne <ahoek@ilionx.com>
2021-01-28 00:44:41 +0100

    Http suggestion

---
+++ rxjs/mocks/http-mock.ts
@@ -0,0 +1,22 @@
+import {Observable, Subject} from 'rxjs';
+import {HttpResponse} from '@angular/common/http';
+
+export class HttpClientMock {
+  responseObject: Subject<HttpResponse<any>> = new Subject<HttpResponse<any>>();
+
+  constructor() {
+
+  }
+
+  observeResponse(): Observable<HttpResponse<any>> {
+    if (this.responseObject.closed) {
+      this.responseObject = new Subject();
+    }
+    return this.responseObject.asObservable();
+  }
+
+  sendResponse(response: HttpResponse<any>): void {
+    this.responseObject.next(response);
+    this.responseObject.complete();
+  }
+}
