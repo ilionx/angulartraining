aee4fbad51ed4310694b6428b603134ca717a249
Michel Vollebregt <mvollebregt@ilionx.com>
2021-03-11 11:56:07 +0100

    Uitwerking control value accessor

--- src/app/main-dish/main-dish.component.ts
+++ src/app/main-dish/main-dish.component.ts
@@ -1,24 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {Dish} from '../shared/model/dish';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {Observable, Subject} from 'rxjs';
+import {map, takeUntil} from 'rxjs/operators';
 import {WizardService} from '../shared/service/wizard.service';
 import {CachedDishService} from '../shared/service/cached-dish.service';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-main-dish',
   templateUrl: './main-dish.component.html'
 })
-export class MainDishComponent {
+export class MainDishComponent implements OnDestroy {
 
   dishes: Observable<Dish[]>;
   selectedMainDish: Observable<Dish | undefined>;
+  mainDishControl = new FormControl();
+
+  private destroy = new Subject<void>();
 
   constructor(dishService: CachedDishService, private wizardService: WizardService) {
     this.dishes = dishService.getDishes().pipe(
       map(dishes => dishes.filter(dish => dish.type === 'main'))
     );
+    this.mainDishControl.valueChanges.pipe(
+      takeUntil(this.destroy)
+    ).subscribe(dish => this.onSelectedDishChange(dish));
     this.selectedMainDish = this.wizardService.getMainDish();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
   }
 
   onSelectedDishChange(dish: Dish): void {
--- src/app/shared/dish-form/dish-form.component.ts
+++ src/app/shared/dish-form/dish-form.component.ts
@@ -1,5 +1,5 @@
-import {Component, EventEmitter, Input, OnDestroy, Output} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {Component, EventEmitter, forwardRef, Input, OnDestroy, Output} from '@angular/core';
+import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {Dish} from '../model/dish';
 import {Subject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
@@ -7,9 +7,14 @@ import {takeUntil} from 'rxjs/operators';
 @Component({
   selector: 'app-dish-form',
   templateUrl: './dish-form.component.html',
-  styleUrls: ['./dish-form.component.scss']
+  styleUrls: ['./dish-form.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => DishFormComponent),
+    multi: true
+  }]
 })
-export class DishFormComponent implements OnDestroy {
+export class DishFormComponent implements OnDestroy, ControlValueAccessor {
 
   @Input() dishes;
   @Output() selectedDishChange = new EventEmitter<Dish>();
@@ -17,11 +22,13 @@ export class DishFormComponent implements OnDestroy {
   dishControl = new FormControl();
 
   private destroy = new Subject<void>();
+  private onChange: any;
+  private onTouched: any;
 
   constructor() {
     this.dishControl.valueChanges.pipe(
       takeUntil(this.destroy)
-    ).subscribe(this.selectedDishChange);
+    ).subscribe(value => this.onSelectedDishChange(value));
   }
 
   ngOnDestroy(): void {
@@ -29,4 +36,23 @@ export class DishFormComponent implements OnDestroy {
     this.destroy.complete();
   }
 
+  writeValue(obj: any): void {
+    this.dishControl.setValue(obj);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  private onSelectedDishChange(value: any): void {
+    this.selectedDishChange.emit(value);
+    if (this.onChange && this.onTouched) {
+      this.onChange(value);
+      this.onTouched();
+    }
+  }
 }
